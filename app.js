663b76e24110404495b96d3d4bf0c4ccba4161e1
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-03-25 01:18:48 +0100

    Add basic app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const nconf = require('nconf');
+const debug = require('debug')('server:app');
+
+nconf
+  .argv()
+  .env()
+  .file({
+    file: './config.json'
+  });
+
+const webOptions = {
+  port: nconf.get('WEB_PORT'),
+  host: nconf.get('WEB_HOST')
+};
+
+const app = express();
+const server = http.createServer();
+
+server.listen(webOptions, function listeningCallback() {
+  debug('server listen on: ', server.address());
+});

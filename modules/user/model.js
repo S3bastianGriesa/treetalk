552737e1a27ecf2bc80d69cecb9a53b112d028d6
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-03-28 12:46:10 +0200

    Remove logical user methods from usermodel

--- modules/user/model.js
+++ modules/user/model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
 
+const userService = require('./service');
+
 const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
@@ -14,8 +16,8 @@ UserSchema
   .virtual('password')
   .set(function setPassword(password) {
     this._password = password;
-    this.salt = this.saltIt();
-    this.hashed_password = this.encryptPassword(password);
+    this.salt = userService.saltIt();
+    this.hashed_password = userService.encryptPassword(password, this);
   })
   .get(function getPassword() {
     return this._password;
@@ -32,24 +34,5 @@ UserSchema
       'role': this.role
     };
   });
-
-UserSchema.methods = {
-  authenticate: function authenticate(password) {
-    return this.encryptPassword(password) === this.hashed_password;
-  },
-
-  saltIt: function saltIt() {
-    return crypto.randomBytes(16).toString('base64');
-  },
-
-  encryptPassword: function encryptPassword(password){
-    if(!password || !this.salt) {
-      return '';
-    }
-
-    const salt = new Buffer(this.salt, 'base64');
-    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
-  }
-};
 
 module.exports = mongoose.model('User', UserSchema);

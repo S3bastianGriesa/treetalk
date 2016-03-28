e1956b3911ce1b534f3aed384ffef50292a18382
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-03-28 15:23:03 +0200

    Add workaround for letting model implement userservice

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,5 +1,9 @@
 const router = require('./router');
 const service = require('./service');
+const model = require('./model');
+
+model.setUserService(service);
 
 module.exports.router = router;
 module.exports.service = service;
+module.exports.model = model;

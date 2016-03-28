709db3ae8de0e6016f9234655bd76f49dbe48e99
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-03-28 12:45:56 +0200

    Add logical user methods and get methods for user to userservice

--- modules/user/service.js
+++ modules/user/service.js
@@ -14,6 +14,35 @@ class UserService {
             .select('_id username email full_name role')
             .exec();
   }
+
+  getUserForID(id) {
+    return UserModel.findById(id).exec();
+  }
+
+  getUserForUsername(username) {
+    return UserModel.findOne({username: username}).exec();
+  }
+
+  getUserForEmail(email) {
+    return UserModel.findOne({email: email}).exec();
+  }
+
+  authenticate(password, user) {
+    return this.encryptPassword(password) === user.hashed_password;
+  }
+
+  saltIt() {
+    return crypto.randomBytes(16).toString('base64');
+  }
+
+  encryptPassword(password, user){
+    if(!password || !user.salt) {
+      return '';
+    }
+
+    const salt = new Buffer(user.salt, 'base64');
+    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+  }
 }
 
 module.exports = new UserService();

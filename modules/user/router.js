b222a7cd4ca3d31fb5a24df4d63443e679fb959d
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-03-28 13:24:17 +0200

    Add deletion route for user

--- modules/user/router.js
+++ modules/user/router.js
@@ -5,7 +5,7 @@ const _ = require('underscore');
 const userService = require('./service');
 const router = express.Router();
 
-router.post('/user/create', (req, res) => {
+router.post('/user', (req, res) => {
   const parameters = _.pick(req.body, 'username', 'email', 'full_name', 'role', 'password');
   debug('try to create user: ' + parameters);
   userService
@@ -20,7 +20,7 @@ router.post('/user/create', (req, res) => {
     });
 });
 
-router.get('/user/getall', (req, res) => {
+router.get('/user', (req, res) => {
   debug('receiving all userdata');
   userService
     .getAllUsers()
@@ -32,6 +32,23 @@ router.get('/user/getall', (req, res) => {
       debug('an error occurred on receiving userdata. ${err}', err);
       res.status(400).send(err);
     });
+});
+
+router.delete('/user', (req, res) => {
+  const id = _.pick(req.body, 'id').id;
+  debug('deleting user ' + id);
+  if(!_.isUndefined(id)){
+    userService
+    .deleteUserForID(id)
+    .then(() => {
+      debug('deleting user successful!');
+      res.status(200).end();
+    })
+    .catch((err) => {
+      debug('an error occurred on receiving userdata. ${err}', err);
+      res.status(400).send(err);
+    });
+  }
 });
 
 module.exports = router;

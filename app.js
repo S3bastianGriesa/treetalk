bfb3fa9a031062b7297329a6db60f7a59f2d7da3
Julian Biermann <julian.bier89@gmail.com>
2016-03-29 13:16:02 +0200

    Added session middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const nconf = require('nconf');
 const debug = require('debug')('server:app');
 const path = require('path');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoSessionStore = require('connect-mongodb-session')(session);
 
 const app = express();
 const server = http.createServer(app);
@@ -15,13 +17,29 @@ nconf
     file: './config.json'
   });
 
+
 const db_url = nconf.get('DB_URL');
 const webOptions = {
   port: nconf.get('WEB_PORT'),
   host: nconf.get('WEB_HOST')
 };
 
+mongoSessionStore = new MongoDBStore({
+  uri: db_url,
+  collection: 'sessions'
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  genid: function(req) {
+    return genuuid();
+  },
+  secret: 'UltraSuperSecretKeyForLulz',
+  cookie: {
+    maxAge: 30 * 60 * 60 * 1000
+  }
+  store: mongoSessionStore
+}));
 
 debug('Try to establish mongdb connection on: ' + db_url);
 mongoose.connect(db_url);

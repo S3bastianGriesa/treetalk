9bcddd25110c936740f4f08b23de06d6950709d2
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-03-31 08:42:35 +0200

    Refactor index.js in user module

--- modules/user/index.js
+++ modules/user/index.js
@@ -1,7 +1,5 @@
-const router = require('./router');
-const service = require('./service');
-const model = require('./model');
-
-module.exports.router = router;
-module.exports.service = service;
-module.exports.model = model;
+module.exports = {
+  router: require('./router'),
+  model: require('./model'),
+  service: require('./service')
+};

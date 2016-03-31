d11ae8327882ec05b5f143f786c11a30a31e016d
Julian Biermann <julian.bier89@gmail.com>
2016-03-31 12:37:53 +0200

    Added crypto-util module

---
+++ modules/crypto-util/crypto-util.js
@@ -0,0 +1,13 @@
+const crypto = require('crypto');
+
+class CryptoUtil {
+  createRandomSalt(size) {
+    crypto.randomBytes(size).toString('hex');
+  }
+
+  createPasswordHash(password, salt) {
+    return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('hex');
+  }
+}
+
+module.exports = new CryptoUtil;

1984a56816fb356f71b38a63d4abcb03ecdfc147
Julian Biermann <julian.bier89@gmail.com>
2016-04-01 13:27:52 +0200

    Added other mongo-session storage engine because the other one was deprecated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const debug = require('debug')('server:app');
 const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoDBSessionStore = require('connect-mongodb-session')(session);
+const MongoDBSessionStore = require('connect-mongo')(session);
 const uid = require('uid-safe');
 const bodyParser = require('body-parser');
 
@@ -23,45 +23,11 @@ nconf
     file: './config.json'
   });
 
-
 const db_url = nconf.get('DB_URL');
 const webOptions = {
   port: nconf.get('WEB_PORT'),
   host: nconf.get('WEB_HOST')
 };
-
-const mongoSessionStore = new MongoDBSessionStore({
-  uri: db_url,
-  collection: 'sessions'
-});
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  genid: function(req) {
-    return uid.sync(18);
-  },
-  secret: uid.sync(18),
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 30 * 60 * 60 * 1000,
-    secure: true
-  },
-  store: mongoSessionStore
-}));
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(user.router);
-app.use(login.router);
-app.get('/app/chat', authentication.middleware.requiresLogin, (req, res) => {
-  res.sendFile('chat.html', {
-    root: './public'
-  });
-});
 
 debug('Try to establish mongdb connection on: ' + db_url);
 mongoose.connect(db_url);
@@ -71,5 +37,34 @@ db.once('open', () => {
   debug('mongodb connection successful established.');
   server.listen(webOptions, function listeningCallback() {
     debug('server listen on: ', server.address());
+  });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(session({
+  genid: function(req) {
+    return uid.sync(18);
+  },
+  secret: 'SuperSecretKeyLulz',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 30 * 60 * 60 * 1000,
+    secure: true
+  },
+  store: new MongoDBSessionStore({
+    mongooseConnection: db
+  })
+}));
+
+app.use(user.router);
+app.use(login.router);
+app.get('/app/chat', authentication.middleware.requiresLogin, (req, res) => {
+  res.sendFile('chat.html', {
+    root: './public'
   });
 });

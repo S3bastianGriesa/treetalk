1c793d526a7f433f37650532ebd79f1917cbd4e4
Julian Biermann <julian.bier89@gmail.com>
2016-04-01 12:03:37 +0200

    Added requireLogin Middleware and set dependency to user module

--- modules/authentication/middleware.js
+++ modules/authentication/middleware.js
@@ -1,5 +1,25 @@
-class AuthenticationMiddleware {
+const debug = require('debug')('server:authentication:middleware');
+const userService = require('user').service;
 
+class AuthenticationMiddleware {
+  requiresLogin(req, res, next) {
+    debug('RequireLogin: Check if Session is valid');
+    if (req.session && req.session.user) {
+      userService.getUserDataByID(req.session.user.id).then(function(user) {
+        if (user) {
+          req.session.user = user;
+          next();
+        } else {
+          res.redirect('/login');
+        }
+      }).fatch(function(err) {
+        console.error(err);
+        res.status(400).send(err);
+      })
+    } else {
+      res.redirect('/login');
+    }
+  }
 }
 
 module.exports = new AuthenticationMiddleware();

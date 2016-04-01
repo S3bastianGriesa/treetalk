ad4b773d4edc25f75fbf49d852b8c0280438eb16
Julian Biermann <julian.bier89@gmail.com>
2016-04-01 11:51:44 +0200

    Added new Login module which handles the routing for the login requests

---
+++ modules/login/router.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const authenticationService = require('authentication');
+
+router.get('/login', (req, res) => {
+  res.sendFile(login.html);
+});
+
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  //TODO: Validate input params.
+  authenticationService.login(email, password).then(function(authentication) {
+    if (authentication.isAuthenticated) {
+      req.session.user = authentication.user;
+      res.redirect('/app/chat');
+    } else {
+      res.redirect('/login');
+    }
+  }).catch(function(err) {
+    console.error(err);
+    res.status(400).send(err);
+  });
+});
+
+module.exports = router;

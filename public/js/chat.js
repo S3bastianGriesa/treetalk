04ad35ec9c7d92bc976be4740afb2372a9584c2d
Julian Biermann <julian.bier89@gmail.com>
2016-04-10 20:11:49 +0200

    Added even and uneven class signing for better chat message overview

--- public/js/chat.js
+++ public/js/chat.js
@@ -21,27 +21,40 @@ ChatWidget.prototype.addMessage = function(msg) {
 };
 
 ChatWidget.prototype.getLatestMessage = function() {
-  return this.messages[this.messages.length - 1];
+    return this.messages[this.messages.length - 1];
+};
+
+ChatWidget.prototype.count = function() {
+    return this.messages.length;
+};
+
+ChatWidget.prototype.isMessageCountEven = function() {
+    return (this.count() % 2) === 0;
 };
 
 ChatWidget.prototype.render = function() {
     var message = $('<li></li>').html(this.getLatestMessage());
+    if (this.isMessageCountEven()) {
+        message.addClass('even');
+    } else {
+        message.addClass('uneven');
+    }
     this.list.append(message);
 };
 
 $(document).ready(function() {
-  var messageWidget = new MessageWidget();
-  var chatWidget = new ChatWidget();
-  var sendButton = $('#send-button');
-  sendButton.click(function(event) {
-    var message = messageWidget.getValue();
-    messageWidget.clear();
-    chatWidget.addMessage(message);
-  });
-  messageWidget.textfield.keypress(function(event) {
-    if(event.which == 13) {
-      sendButton.click();
-      return false;
-    }
-  });
+    var messageWidget = new MessageWidget();
+    var chatWidget = new ChatWidget();
+    var sendButton = $('#send-button');
+    sendButton.click(function(event) {
+        var message = messageWidget.getValue();
+        messageWidget.clear();
+        chatWidget.addMessage(message);
+    });
+    messageWidget.textfield.keypress(function(event) {
+        if (event.which == 13) {
+            sendButton.click();
+            return false;
+        }
+    });
 });

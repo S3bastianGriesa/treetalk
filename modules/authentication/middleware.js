b177142361b83c4c8df4bb03830b32b397d36471
Julian Biermann <julian.bier89@gmail.com>
2016-04-10 15:30:16 +0200

    Changed the review comments

--- modules/authentication/middleware.js
+++ modules/authentication/middleware.js
@@ -2,23 +2,25 @@ const debug = require('debug')('server:authentication:middleware');
 const userService = require('user').service;
 
 class AuthenticationMiddleware {
-  requiresLogin(req, res, next) {
-    if (req.session && req.session.user) {
-      userService.getUserDataByID(req.session.user.id).then(function(user) {
-        if (user) {
-          req.session.user = user;
-          next();
+    requiresLogin(req, res, next) {
+        if (req.session && req.session.user) {
+            userService.getUserDataByID(req.session.user.id)
+                .then((user) => {
+                    if (user) {
+                        req.session.user = user;
+                        next();
+                    } else {
+                        res.redirect('/login');
+                    }
+                })
+                .catch(function(err) {
+                    console.error(err);
+                    res.status(400).send(err);
+                });
         } else {
-          res.redirect('/login');
+            res.redirect('/login');
         }
-      }).catch(function(err) {
-        console.error(err);
-        res.status(400).send(err);
-      });
-    } else {
-      res.redirect('/login');
     }
-  }
 }
 
 module.exports = new AuthenticationMiddleware();
--- modules/login/router.js
+++ modules/login/router.js
@@ -2,32 +2,34 @@ const router = require('express').Router();
 const authenticationService = require('authentication').service;
 
 router.get('/login', (req, res) => {
-  res.sendFile('/login.html', {
-    root: './public'
-  });
+    res.sendFile('/login.html', {
+        root: './public'
+    });
 });
 
 router.post('/login', (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  authenticationService.login(email, password).then(function(authentication) {
-    if (authentication.isAuthenticated) {
-      req.session.user = authentication.user;
-      res.redirect('/app/chat');
-    } else {
-      res.redirect('/login');
-    }
-  }).catch(function(err) {
-    console.error(err);
-    res.status(400).send(err);
-  });
+    const email = req.body.email;
+    const password = req.body.password;
+    authenticationService.login(email, password)
+        .then((authentication) => {
+            if (authentication.isAuthenticated) {
+                req.session.user = authentication.user;
+                res.redirect('/app/chat');
+            } else {
+                res.redirect('/login');
+            }
+        })
+        .catch(function(err) {
+            console.error(err);
+            res.status(400).send(err);
+        });
 });
 
 router.get('/logout', (req, res) => {
-  if (req.session) {
-    req.session.destroy();
-  }
-  res.redirect('/login');
+    if (req.session) {
+        req.session.destroy();
+    }
+    res.redirect('/login');
 });
 
 module.exports = router;

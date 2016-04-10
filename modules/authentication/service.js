a0332182132deacfdaa651150248d287d36d554a
Julian Biermann <julian.bier89@gmail.com>
2016-04-10 15:34:13 +0200

    Review changes

--- modules/authentication/service.js
+++ modules/authentication/service.js
@@ -3,27 +3,28 @@ const userService = require('user').service;
 const cryptoUtil = require('crypto-util');
 
 class AuthenticationService {
-  login(email, password) {
-    return userService.getUserByEmail(email).then(function(user) {
-      let isAuthenticated = false;
+    login(email, password) {
+        return userService.getUserByEmail(email)
+            .then((user) => {
+                let isAuthenticated = false;
 
-      if (user) {
-        const salt = user.salt;
-        const hashedPassword = cryptoUtil.createPasswordHash(password,
-          salt);
+                if (user) {
+                    const salt = user.salt;
+                    const hashedPassword = cryptoUtil.createPasswordHash(password,
+                        salt);
 
-        isAuthenticated = user.hashed_password == hashedPassword;
+                    isAuthenticated = user.hashed_password == hashedPassword;
 
-        user.salt = '';
-        user.hashed_password = '';
-      }
+                    user.salt = '';
+                    user.hashed_password = '';
+                }
 
-      return {
-        isAuthenticated: isAuthenticated,
-        user: user
-      };
-    });
-  }
+                return {
+                    isAuthenticated: isAuthenticated,
+                    user: user
+                };
+            });
+    }
 }
 
 module.exports = new AuthenticationService();

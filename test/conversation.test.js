8c1a08aa6c7d5a0447ad226781cdf7e011793110
Julian Biermann <julian.bier89@gmail.com>
2016-04-19 16:54:07 +0200

    Changed unit tests

--- test/conversation.test.js
+++ test/conversation.test.js
@@ -1,6 +1,7 @@
 const supertest = require('supertest');
 const should = require('should');
 const server = supertest.agent('http://localhost:8000'); //TODO: [JBI] Create a common test case which uses these dependencies
+const _ = require('underscore');
 
 describe('Conversation Module', () => {
     before(() => {
@@ -19,11 +20,10 @@ describe('Conversation Module', () => {
     });
 
     describe('POST /conversations', () => {
-        it('Should create a new Conversation and return it', (done) => {
+        it('Should create a new conversation and return it', (done) => {
             const conversation = {
                 title: 'Mocha Test Conversation',
-                access: 'public',
-                owner: 'TEST' //TODO: [jbi] Find out how to get a test user
+                access: 'public'
             };
 
             server
@@ -33,81 +33,86 @@ describe('Conversation Module', () => {
                 .expect(200)
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.data.should.equal(conversation);
+                    res.data.title.should.equal(conversation.title);
+                    res.data.access.should.equal(conversation.access);
+                    res.data.owner.should.not.be.null();
+                    res.data.members.length.should.equal(1);
                     done();
                 });
         });
     });
 
     describe('PUT /conversations/:id', () => {
-        it('Should update a Conversation with new properties and return it', (done) => {
+        it('Should update a conversation with new properties and and return the old data it', (done) => {
 
         });
     });
 
-    describe('GET /users/:userId/conversations', () => {
-        it('Should return all Conversations where the User is a member of', (done) => {
+    describe('GET /conversations', () => {
+        it('Should return all conversations where the user is a member of', (done) => {
             server
                 .get('/users/' + userId + '/conversations')
                 .expect('Content-Type', /json/)
                 .expect(200)
                 .end((err, res) => {
-                  should.not.exists(err);
-                  res.should.have.status(200);
-                  res.data.should.not.be.null();
-                  done();
+                    should.not.exists(err);
+                    res.should.have.status(200);
+                    res.data.should.not.be.null();
+                    done();
                 });
         });
 
-        it('Should return an Error when the URL Parameter :userId is not the same as the logged in User ID', (done) => {
-          server
-          .get('/users/randomUserIdWhichShouldNotExist/conversations')
-          .expect(503)
-          .end((err, res) => {
-            should.exists(err);
-            res.should.have.status(503);
-            done();
-          });
+        it('Should return all conversations with public access', (done) => {
+            server
+                .get('/app/conversations?filter=public')
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    should.not.exists(err);
+                    res.should.have.status(200);
+                    res.data.should.not.be.null();
+                    //TODO: [JBI] iterate over array and validate with should
+                    res.data[0].access.should.equal('public');
+                    done();
+                });
         });
     });
 
     describe('GET /conversations/:id', () => {
-        it('Should return a Conversation where the User is a member of', (done) => {
+        let testConversation = null;
 
+        before(() => {
+            const testConversationData = {
+                title: 'TEST GET /conversation/:id',
+                access: 'public'
+            };
+
+            server
+                .post('/app/conversations')
+                .send(testConversationData)
+                .end((err, res) => {
+                    testConversation = res.data;
+                });
         });
 
-        it('Should return a Conversation where the User is not a member of but the User has an administrator role', (done) => {
-
+        it('Should return a conversation where the user is a member of', (done) => {
+            server
+                .get('/app/conversations/' + testConversation._id)
+                .expect(200)
+                .expect('Content-Type', /json/)
+                .end((err, res) => {
+                    should.not.exists(err);
+                    res.data._id.should.equals(testConversation._id);
+                    res.data.title.should.equals(testConversation.title);
+                    res.data.access.should.equals(testConversation.access);
+                    done();
+                });
         });
 
-        it('Should return an Error when the User is not a member of the Conversation and has no admin role', (done) => {
-
-        });
-
-        it('Should return an Error when the Conversation is 1 to 1 and the User is not a member of it', (done) => {
-
+        it('Should return an Error when the user is not a member of the conversation', (done) => {
         });
     });
 
     describe('DELETE /conversations/:id', () => {
-        it('Should delete a Conversation when the User is the owner of it', (done) => {
-
-        });
-
-        it('Should delete a Conversation when the User has the admin role', (done) => {
-
-        });
-
-        it('Should delete a 1 to 1 Conversation when the User is a member of it', (done) => {
-
-        });
-
-        it('Should return an Error when the Conversation is a 1 to 1 Conversation and the User is no member of it', (done) => {
-
-        });
-
-        it('Should return an Error when the User is not the Owner of the Conversation', (done) => {
-
-        });
     });
 });
